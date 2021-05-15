6e7763965d3f82dad1b23ae211b47e4ada8489cb
WajidAli3301 <55339939+WajidAli3301@users.noreply.github.com>
2021-05-15 18:12:48 +0800

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,30 @@
+# Create a bucket
+resource "aws_s3_bucket" "alsaaaaaaaabmnbmnbitest1-1990-1010213" {
+
+  versioning {
+    enabled = true
+  }
+  bucket = "testing3301-dash"
+  tags = {     
+    Name = "terratest-aws-s3-example-3301"     
+    Environment = "Dev"   
+  }
+
+}
+
+# Upload an object
+resource "aws_s3_bucket_object" "test1-creation" {
+
+  bucket = aws_s3_bucket.alsaaaaaaaabmnbmnbitest1-1990-1010213.id
+
+  key    = "test1.txt"
+  content = "${timestamp()}"
+}
+
+resource "aws_s3_bucket_object" "test2-creation" {
+
+  bucket = aws_s3_bucket.alsaaaaaaaabmnbmnbitest1-1990-1010213.id
+
+  key    = "test2.txt"
+  content = "${timestamp()}"
+}
