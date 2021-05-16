ba46e3d6b3cf10de873ad0245d16dca720177b46
WajidAli3301 <55339939+WajidAli3301@users.noreply.github.com>
2021-05-16 16:11:35 +0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {    
-  access_key = "${{ secrets.AWS_ACCESS_KEY_ID }}"    
-  secret_key = "${{ secrets.AWS_SECRET_ACCESS_KEY }}"    
+  access_key = "AKIA4RZ5UFCANFVAVDNA"    
+  secret_key = "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"    
   region     = "us-east-1" 
 }
 
